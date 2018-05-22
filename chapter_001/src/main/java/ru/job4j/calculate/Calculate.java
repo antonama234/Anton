671435a48ebf730a0bc18be1e21783267a21c21e
Anton <ymarkovskii1986@mail.ru>
2018-05-22 22:33:25 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculate;
 
+	/**
+	 * Это класс выводит на консоль строчку "Hello World!".
+	 * @autor Anton.
+	 */
+	 
 	public class Calculate {
+		
+		/**
+		* Метод main.
+		* @param args - args.
+		*/
 		public static void main(String[] args) {
 			System.out.println("Hello World!");
 		}
