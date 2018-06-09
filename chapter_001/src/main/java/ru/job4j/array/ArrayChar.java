4e9c6cbd9319d2134425646af70280b7ad043bfd
Anton <ymarkovskii1986@mail.ru>
2018-06-09 21:29:17 +0300

    6.0. Заполнить массив степенями чисел. [#33488]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    private char[] data;
+
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+        for (int i = 0; i != value.length; i++) {
+            for (int j = 0; j != data.length; j++) {
+                if (data[i] != value[i]) {
+                    result = false;
+                }
+            }
+        }
+        return result;
+    }
+}
