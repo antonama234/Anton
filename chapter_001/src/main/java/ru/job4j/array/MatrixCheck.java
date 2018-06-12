9ae52527ce08d8c5b4e12b394f039df64199eb85
Anton <ymarkovskii1986@mail.ru>
2018-06-12 13:49:30 +0300

    6.0. Заполнить массив степенями чисел. [#33488]

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class MatrixCheck {
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        for (int i = 0; i != data.length; i++) {
+            for (int j = 0; j != data.length; j++) {
+                if (data[0][0] != data[data.length - 1 - j][data.length - 1 - j]) {
+                    result = false;
+                } else if (data[0][data.length - 1] != data[i][data.length - 1 - i]) {
+                    result = false;
+                }
+            }
+        }
+        return result;
+    }
+}
