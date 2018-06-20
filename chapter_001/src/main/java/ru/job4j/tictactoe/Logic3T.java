c477e4523008977ea8c91bedc07ad5c924313b31
Anton <ymarkovskii1986@mail.ru>
2018-06-20 22:12:06 +0300

    Крестики-нолики на JavaFX [#49777]

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -8,27 +8,23 @@ public class Logic3T {
     }
 
     public boolean isWinnerX() {
-        boolean X = false;
-        for (int i = 0; i != table.length; i++) {
-            for (int j = 0; j != table.length; j++) {
-                if (table[table.length - 1][0] == table[table.length - 1][i] || table[0][table.length - 1] == table[j][table.length - 1] || table[0][table.length - 2] == table[i][table.length - 2] || table[table.length - 2][0] == table[table.length - 2][j] || table[0][0] == table[table.length - 1 - i][0] || table[0][0] == table[0][table.length - 1 - i] ||table[0][0] == table[table.length - 1 - j][table.length - 1 - j] || table[0][table.length - 1] == table[i][table.length - 1 - i]) {
-                    X = true;
-                }
-            }
-        }
-        return X;
+        return isWinner(true);
     }
 
     public boolean isWinnerO() {
-        boolean O = false;
+        return isWinner(false);
+    }
+
+    public boolean isWinner(boolean checCell) {
+        boolean result = false;
         for (int i = 0; i != table.length; i++) {
             for (int j = 0; j != table.length; j++) {
-                if (table[table.length - 1][0] == table[table.length - 1][i] || table[0][table.length - 1] == table[j][table.length - 1] ||table[0][table.length - 2] == table[i][table.length - 2] || table[table.length - 2][0] == table[table.length - 2][j] || table[0][0] == table[table.length - 1 - i][0] || table[0][0] == table[0][table.length - 1 - i] ||table[0][0] == table[table.length - 1 - j][table.length - 1 - j] || table[0][table.length - 1] == table[i][table.length - 1 - i]) {
-
+                if (table[table.length - 1][0] == table[table.length - 1][i] || table[0][table.length - 1] == table[j][table.length - 1] || table[0][table.length - 2] == table[i][table.length - 2] || table[table.length - 2][0] == table[table.length - 2][j] || table[0][0] == table[table.length - 1 - i][0] || table[0][0] == table[0][table.length - 1 - i] || table[0][0] == table[table.length - 1 - j][table.length - 1 - j] || table[0][table.length - 1] == table[i][table.length - 1 - i]) {
+                    result = true;
                 }
             }
         }
-        return O;
+        return result;
     }
 
     public boolean hasGap() {
