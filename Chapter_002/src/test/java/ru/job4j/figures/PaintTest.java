9a9f71489de02a1de9df187cedfcb7cd2ff0551f
Anton <ymarkovskii1986@mail.ru>
2018-07-25 21:12:20 +0300

    5. Рефакторинг теста @Before @After [#33568]

--- Chapter_002/src/test/java/ru/job4j/figures/PaintTest.java
+++ Chapter_002/src/test/java/ru/job4j/figures/PaintTest.java
@@ -1,36 +1,60 @@
 package ru.job4j.figures;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        String ls = System.lineSeparator();
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
-                new String(out.toByteArray()),
+                new String(this.out.toByteArray()),
                 is(
-                        new StringBuilder()
-                                .append("XXXXX")
-                                .append(ls)
-                                .append("X   X")
-                                .append(ls)
-                                .append("X   X")
-                                .append(ls)
-                                .append("X   X")
-                                .append(ls)
-                                .append("XXXXX")
-                                .append(ls)
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("XXXXX")
+                                .add("X   X")
+                                .add("X   X")
+                                .add("X   X")
+                                .add("XXXXX")
                                 .toString()
                 )
         );
-        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Tringle());
+        assertThat(
+                new String(this.out.toByteArray()),
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   X   ")
+                                .add("  XXX  ")
+                                .add(" XXXXX ")
+                                .add("XXXXXXX")
+                                .toString()
+                )
+        );
     }
 }
