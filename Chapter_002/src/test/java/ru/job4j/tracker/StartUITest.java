2c7dc109553e99c918508eb80963884a3ceeaeb9
Anton <ymarkovskii1986@mail.ru>
2018-07-30 14:40:23 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль. [#33585]

--- Chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ Chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -27,20 +27,36 @@ public class StartUITest {
     @Test
     public void whenGetAllThenShowAllItems() {
         Item first = tracker.add(new Item("testId", "testName", "testDescription"));
-        Item second = tracker.add(new Item("testId2", "testName", "testDescription2"));
-        Input input = new StubInput(new String[]{"2"});
+        Item second = tracker.add(new Item("testId2", "testName2", "testDescription2"));
+        Input input = new StubInput(new String[]{"2", "7"});
         new StartUI (input, tracker).init();
         assertThat(
             new String(this.out.toByteArray()),
                 is(new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                        .add("Меню.")
+                        .add("1. Создание новой заявки.")
+                        .add("2. Отобразить все заявки.")
+                        .add("3. Отредактировать заявку.")
+                        .add("4. Удалить заявку.")
+                        .add("5. Найти заявку по ID")
+                        .add("6. Найти заявку по имени.")
+                        .add("7. Выход из приложения.")
                         .add("---------------------- Заявки ----------------------")
-                        .add("ID заявки: testId")
+                        .add("ID заявки: " + tracker.getAll()[0].getId())
                         .add("Наименование заявки: testName")
                         .add("Описание заявки: testDescription")
-                        .add("ID заявки: testId2")
+                        .add("ID заявки: " + tracker.getAll()[1].getId())
                         .add("Наименование заявки: testName")
                         .add("Описание заявки: testDescription2")
                         .add(" ")
+                        .add("Меню.")
+                        .add("1. Создание новой заявки.")
+                        .add("2. Отобразить все заявки.")
+                        .add("3. Отредактировать заявку.")
+                        .add("4. Удалить заявку.")
+                        .add("5. Найти заявку по ID")
+                        .add("6. Найти заявку по имени.")
+                        .add("7. Выход из приложения.")
                         .toString()
                 )
 
