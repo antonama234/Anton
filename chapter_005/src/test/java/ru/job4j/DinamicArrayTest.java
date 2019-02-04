7d3e9473654ce2214a9c62f69067f2853af38229
Anton <ymarkovskii1986@mail.ru>
2019-02-04 22:01:56 +0300

    5.3.1. Создать динамический список на базе массива.[#99500]

--- chapter_005/src/test/java/ru/job4j/DinamicArrayTest.java
+++ chapter_005/src/test/java/ru/job4j/DinamicArrayTest.java
@@ -3,6 +3,9 @@ package ru.job4j;
 import org.junit.Test;
 import ru.job4j.list.DinamicArray;
 
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -14,5 +17,15 @@ public class DinamicArrayTest {
         dinamicArray.add(1);
         dinamicArray.add(2);
         assertThat(dinamicArray.get(0), is(1));
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenIterateNextThenHaveException() {
+        dinamicArray.add(1);
+        dinamicArray.add(2);
+        Iterator iterator = dinamicArray.iterator();
+        assertThat(iterator.next(), is(1));
+        dinamicArray.add(3);
+        iterator.next();
     }
 }
