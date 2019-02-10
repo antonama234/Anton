bf0af4a406f77eb3cd555e2fecc8968ee68a7581
Anton <ymarkovskii1986@mail.ru>
2019-02-10 10:43:46 +0300

    5.3.2. Создать контейнер на базе связанного списка [#99501]

---
+++ chapter_005/src/main/java/ru/job4j/list/DinamicLinkedList.java
@@ -0,0 +1,59 @@
+package ru.job4j.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DinamicLinkedList<E> implements Iterable<E> {
+    private Node<E> first;
+    private int size;
+    private int modCount;
+
+    public void add(E value) {
+        Node<E> newLink = new Node<>(value);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+        this.modCount++;
+    }
+
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.date;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int position;
+            int expectedModCount =  modCount;
+            @Override
+            public boolean hasNext() {
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return position < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return get(position++);
+            }
+        };
+    }
+
+    private static class Node<E> {
+        E date;
+        Node<E> next;
+
+        Node(E date) {
+            this.date = date;
+        }
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/DinamicLinkedListTest.java
@@ -0,0 +1,33 @@
+package ru.job4j;
+
+import org.junit.Before;
+import org.junit.Test;
+import ru.job4j.list.DinamicLinkedList;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DinamicLinkedListTest {
+    private DinamicLinkedList<Integer> list = new DinamicLinkedList<>();
+
+    @Test
+    public void whenAddThreeElementsThenUseGetOneResultTwo() {
+        list.add(1);
+        list.add(2);
+        assertThat(list.get(1), is(1));
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenIterateNextThenHaveException() {
+        list.add(1);
+        Iterator iterator = list.iterator();
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is(1));
+        assertThat(iterator.hasNext(), is(false));
+        list.add(1);
+        iterator.next();
+    }
+}
