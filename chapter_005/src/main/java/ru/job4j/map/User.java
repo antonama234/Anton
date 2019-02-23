70adced96dfc4418ed57674b4f3528b2fa8387fc
Anton <ymarkovskii1986@mail.ru>
2019-02-23 14:47:32 +0300

    1. Создать модель User[#99518]

---
+++ chapter_005/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,27 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+public class User {
+    String name;
+    int children;
+    Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+}
