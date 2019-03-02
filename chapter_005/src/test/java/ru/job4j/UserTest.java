6adff6b0472e72772a830900f3b167c28197b24a
Anton <ymarkovskii1986@mail.ru>
2019-03-02 19:04:29 +0300

    2. Не перекрывать equals hashCode[#99522]

---
+++ chapter_005/src/test/java/ru/job4j/UserTest.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+import org.junit.Test;
+import ru.job4j.map.User;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserTest {
+
+    @Test
+    public void whenAddTwoSameUsersThenHaveTwoDifferentHashCode() {
+    Map<User, Object> map = new HashMap<>();
+    User first = new User("Max", 2, new GregorianCalendar(2001, 01, 01));
+    User second = new User("Max", 2, new GregorianCalendar(2001, 01, 01));
+    map.put(first, new Object());
+    map.put(second, new Object());
+    System.out.println(map);
+    }
+}
